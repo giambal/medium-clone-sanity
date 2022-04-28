33e11a052837a9c3dc27c06f6340b7ef5c52c2b6
GiamBal <gimmy_1996@hotmail.it>
2022-04-28 17:22:33 +0200

    homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 import Header from '../components/Header'
 import { sanityClient, urlFor } from '../sanity'
 import { Post } from '../typings'
@@ -59,6 +60,36 @@ export default function Home({ posts }: Props) {
             alt=""
           />
         </div>
+      </div>
+
+      {/* posts */}
+      <div className="grid grid-cols-1 gap-3 p-3 sm:grid-cols-2 md:gap-6 lg:grid-cols-3 lg:p-6">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="group cursor-pointer rounded-lg border">
+              <div className="overflow-hidden">
+                <img
+                  className="h-60 w-full object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
+                  src={urlFor(post.mainImage).url()!}
+                  alt=""
+                />
+              </div>
+              <div className="flex items-center justify-between bg-white p-5">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs font-light">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   )
--- sanity.js
+++ sanity.js
@@ -3,6 +3,7 @@ import {
   createCurrentUserHook,
   createClient,
 } from 'next-sanity'
+import imageUrlBuilder from '@sanity/image-url'
 
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
@@ -13,4 +14,8 @@ export const config = {
 }
 
 export const sanityClient = createClient(config)
-export const urlFor = (source) => createImageUrlBuilder(config).image(source)
+
+const builder = imageUrlBuilder(config)
+export function urlFor(source) {
+  return builder.image(source)
+}
