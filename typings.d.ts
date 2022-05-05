5ab6939d58cbbdb68d899f4979adcf3b907bd0ca
GiamBal <gimmy_1996@hotmail.it>
2022-05-05 22:14:30 +0200

    comment api + schema + interface

--- typings.d.ts
+++ typings.d.ts
@@ -5,6 +5,9 @@ export interface Post {
   author: {
     name: string
     image: string
+    slug: {
+      current: string
+    }
   }
   description: string
   mainImage: {
@@ -16,4 +19,31 @@ export interface Post {
     current: string
   }
   body: [object]
+  comments: [Comment]
+}
+
+export interface Author {
+  _id: string
+  name: string
+  slug: {
+    current: string
+  }
+  image: string
+  bio: [object]
+}
+
+export interface Comment {
+  approved: boolean
+  comment: string
+  email: string
+  name: string
+  post: {
+    ref: string
+    _type: string
+  }
+  _createdAt: string
+  _id: string
+  _rev: string
+  _type: string
+  _updatedAt: string
 }
